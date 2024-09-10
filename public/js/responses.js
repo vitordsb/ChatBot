d1b88ae46864d3d59b6370f73bf74a6f482206e5
vitordsb <vitordsb2019@gmail.com>
2024-09-10 12:52:05 -0300

    new features

--- public/js/responses.js
+++ public/js/responses.js
@@ -1,23 +1,4 @@
 export const arrayOfPossibleMessage = [
-    {mensagem:"Jarvis, ta aí?", response: "Pra você sempre senhor!"},
-    {mensagem:"Jarvis, ta ai?", response: "Pra você sempre senhor!"},
-    {mensagem:"Olá", response: "hello!"},
-    {mensagem:"kkkkkkkkk", response: "ta rindo de que poha?"},
-    {mensagem:"Quem é o cara mais gay do mundo?", response: "Sem dúvidas é o gustavo!"},
-    {mensagem:"Quem é o mineiro", response: "É o pedro do 1 semestre"},
-    {mensagem:"oi", response: "Olá!"},
-    {mensagem:"como vai?", response: "Bem e vc?"},
-    {mensagem:"boa tarde", response: "Tarde boa!"},
-    {mensagem:"boa noite", response: "Noite boa!"},
-    {mensagem:"Quem é você?", mensagem: "quem é vc?", response: "Sou o Jarvis, seu assistente virtual pessoal, me diga o que precisa, que eu auxiliarei!"},
-    {mensagem:"tudo bem?", response: "Estou bem, e você?"},
-    {mensagem:"preciso de ajuda", response: "Por favor, me diga o que preciso fazer!"},
-    {mensagem:"bom dia", response: "Dia bom!"},
-    {mensagem:"boa manhã", response: "Boa!"},
-    {mensagem:"preciso de um novo desafio", response: "Tudo bem! Como posso te ajudar?"},
-    {mensagem:"Quem é a mulher mais linda do mundo?", response: "Senhor, estou entre a Victorya, e a Victorya"},
-    {mensagem:"como pode me ajudar?", response: "Eu posso te ajudar com tudo que você precisar, como por exemplo, responder perguntas, fazer pesquisas, ou ler artigos. Sempre me diga o que precisa, e eu te ajudarei!"},
-    {mensagem:"preciso de uma nova tarefa", response: "Tudo bem! Como posso te ajudar?"},
-
-
+    {mensagem:"ola", response: "Olá Distribuidor, sou a electra, a assistente virtual da EGP IND. E COM. LTDA."},
+    {mensagem:"quero comprar cerca eletrica", response: "Perfeito, nós somos a melhor fabricante de cerca elétrica e eletrificadoras do país, tanto nacional, quanto latina."},
 ] 
--- public/js/script.js
+++ public/js/script.js
@@ -9,8 +9,16 @@ let user = {mensagem:""};
 async function sendMessage(userMessage) {
    let messageElement = document.createElement('div');
     messageElement.style.textAlign = 'right'
+    messageElement.style.width = '300px';
+    messageElement.style.position = 'relative';
+    messageElement.style.left = '320px';
+    messageElement.style.fontSize = '20px';
+    messageElement.style.backgroundColor = 'white';
+    messageElement.style.padding = '10px';
+    messageElement.style.borderRadius = '15px';
+    messageElement.style.border = '1px solid black';
     messageElement.style.margin = '10px';
-   messageElement.innerHTML = "<span> Você: </span>" +
+   messageElement.innerHTML = "<span> Distribuidor: </span>" +
                         "<span>" + userMessage +"</span>"
 
     chatContainer.appendChild(messageElement);
@@ -23,40 +31,68 @@ async function chatBotResponse(userMessage) {
     let result = arrayOfPossibleMessage.find(val => userMessage.toLowerCase().includes(val.mensagem.toLowerCase()));
 
     if (result) {
-        chatBotMessage = result.response.toLowerCase();
+        chatBotMessage = result.response;
     }
 
     if (chatBotMessage !== "") {
         let messageElement = document.createElement('div');
         messageElement.style.margin = '10px';
+        messageElement.style.fontSize = '20px';
+        messageElement.style.fontWeight = 'bolder';
+        messageElement.style.width = '400px';
+        messageElement.style.textAlign = 'justify';
+        messageElement.style.backgroundColor = 'white';
+        messageElement.style.padding = '10px';
+        messageElement.style.borderRadius = '15px';
+        messageElement.style.border = '1px solid black';
         messageElement.style.color = 'blue';
-        messageElement.innerHTML = "<span>Jarvis: </span>" +
-            "<span>" + chatBotMessage + "</span>";
+        messageElement.innerHTML ="<span> Electra: </span>" +
+            "<span>"+chatBotMessage+"</span>";
 
         chatContainer.appendChild(messageElement);
     }
     else {
         let messageElement = document.createElement('div');
         messageElement.style.margin = '10px';
+        messageElement.style.fontSize = '20px';
+        messageElement.style.fontWeight = 'bolder';
+        messageElement.style.width = '400px';
+        messageElement.style.textAlign = 'justify';
+        messageElement.style.backgroundColor = 'white';
+        messageElement.style.padding = '10px';
+        messageElement.style.borderRadius = '15px';
+        messageElement.style.border = '1px solid black';
         messageElement.style.color = 'red';
-        messageElement.innerHTML = "<span>Jarvis: </span>" +
+        messageElement.innerHTML = "<span>Electra: </span>" +
             "<span>Desculpe, não entendi o que você quis dizer.</span>";
 
         chatContainer.appendChild(messageElement);
     }
 }
+
+
+function escolher(userMessage, chatBotMessage) {
+    
+    if (userMessage == "oi"){
+        chatBotMessage = messageElement.innerHTML = document.createElement = "<span> Escolha uma das opções abaixo: </span>" + "<span> Digite 1 para ver os produtos.</span> \n Digite 2 para pegar os contatos comerciais"
+        chatContainer.appendChild(messageElement);
+    }    
+}
+
 function handleSend() {
     let userMessage = textBox.value;
 
-    if (userMessage === "") {
+    if (userMessage == "") {
         alert("Por favor, digite uma mensagem!");
-    } else {
+    }
+    else {
         let userMessageText = userMessage.trim();
         user.mensagem = userMessageText;
         textBox.value = "";
         sendMessage(userMessageText);
         chatBotResponse(userMessageText);
     }
+
 }
 
 sendBtn.addEventListener('click', handleSend);
